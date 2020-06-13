3f97c191a21710b321916561976ada8e50a11a8c
retroazure <tomascosta005@outlook.com>
2020-06-13 23:50:07 +0100

    task 2

--- js/index.js
+++ js/index.js
@@ -42,7 +42,61 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let headerImg = document.getElementById("cta-img");
-headerImg.setAttribute('src', "img/header-img.png");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"]);
 
 let middleImg = document.getElementById("middle-img");
-middleImg.setAttribute('src', "img/mid-page-accent.jpg");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+for(let i = 1; i<7; i++){
+  document.querySelector("nav a:nth-child("+i+")").innerHTML = siteContent["nav"]["nav-item-"+i+""];
+  document.querySelector("nav a:nth-child("+i+")").style.color = "green";
+  
+}
+
+let ctaHeader = document.querySelector("h1");
+ctaHeader.innerHTML = siteContent["cta"]["h1"];
+
+let ctaButton = document.querySelector("button");
+ctaButton.innerHTML = siteContent["cta"]["button"];
+
+let textContent = document.querySelector(".text-content h4");
+textContent.innerHTML = siteContent["main-content"]["features-h4"];
+
+let paragraphContent = document.querySelector(".text-content p");
+paragraphContent.innerHTML = siteContent["main-content"]["features-content"];
+
+let textContent2 = document.querySelector("body > div > section.main-content > div.top-content > div:nth-child(2) > h4");
+textContent2.innerHTML = siteContent["main-content"]["about-h4"];
+
+let paragraphContent2 = document.querySelector("body > div > section.main-content > div.top-content > div:nth-child(2) > p");
+paragraphContent2.innerHTML = siteContent["main-content"]["about-content"];
+
+let textContent3 = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(1) > h4");
+textContent3.innerHTML = siteContent["main-content"]["services-h4"];
+
+let paragraphContent3 = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(1) > p");
+paragraphContent3.innerHTML = siteContent["main-content"]["services-content"];
+
+let textContent4 = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(2) > h4");
+textContent4.innerHTML = siteContent["main-content"]["product-h4"];
+
+let paragraphContent4 = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(2) > p");
+paragraphContent4.innerHTML = siteContent["main-content"]["product-content"];
+
+let textContent5 = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(3) > h4");
+textContent5.innerHTML = siteContent["main-content"]["vision-h4"];
+
+let paragraphContent5 = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(3) > p");
+paragraphContent5.innerHTML = siteContent["main-content"]["vision-content"];
+
+let contactText = document.querySelector(".contact h4");
+
+contactText.innerHTML = siteContent["contact"]["contact-h4"];
+
+let contactP = document.querySelector(".body > div > section.contact > p:nth-child(2)");
+
+contactP.innerHTML = siteContent["contact"]["address"];
+
+
+
